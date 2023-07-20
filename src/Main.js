c212a5e830224f5c2f1210b6a3f96d078dee9e3f
Hanna Golikova <hgolikov@afilias.info>
2023-07-20 12:00:04 +0100

    first commit

---
+++ src/Main.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Page = () => {
+  return <div>
+    <h1>Hanna Golikova</h1>
+    <p>Dublin, Ireland</p>
+
+    <img src="/hanna_golikova.jpg" alt="Hanna Golikova" width="200" height="auto" className="twirl-animation"/>
+
+    <h2>Senior Software Developer</h2>
+    <p>Senior Software Engineer with more than 13 years of professional experience and a wide skill set. Proficient in both clientside and back end development.</p>
+    <p><a href="https://www.linkedin.com/in/hanna-golikova-64a2595b/">LinkedIn Profile</a></p>
+
+    <h2>Yoga Instructor and co-founder of Fuego Flow - Dance & Yoga</h2>
+    <p><a href="https://www.fuegoflow.ie/">Fuego Flow - Dance & Yoga</a></p>
+  </div>;
+};
+
+export default Page;
